ede7a0b7a5a9065642255d755d92b33880564955
impressivecoder1 <rohanrahman251@gmail.com>
2025-10-30 02:27:24 +0600

    added

--- class1.js
+++ class1.js
@@ -1,1 +1,24 @@
 //(optional) explore what you can do with javascript object
+const student = {
+    name: 'sakib',
+    id: 100,
+    address: 'dhaka',
+    isMarried: true,
+    friends: ['a', 'b', 'c', 'd', 's'],
+    movies: [{name: '1', year: 2020, income: 0}, {name: '2', year: 3030, income: 33}],
+    car: {
+        brand: 'tesla',
+        price: 444444444,
+        made: 2020,
+        manufacturer: {
+            name: 'tesla',
+            ceo: 'elon mask',
+            country: 'usa'
+        }
+    }
+}
+console.log(student)
+console.log(student.car)
+console.log(student.friends)
+console.log(student.car.manufacturer)
+console.log(student.movies[0].name)
