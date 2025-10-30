cc3db03a79b931fc651e8d51bd68cdc78ab61d9a
impressivecoder1 <rohanrahman251@gmail.com>
2025-10-30 23:20:17 +0600

    class2 MOST IMPORTANT CLASS

--- class2.js
+++ class2.js
@@ -11,11 +11,16 @@ for(const number of numbers){
 
 const products = [
     {id: 1, name: 'walton phone', price: 190007},
-    {id: 2, name: 'walton2', price: 190006},
+    {id: 2, name: 'walton2 Phone', price: 190006},
     {id: 3, name: 'walton3', price: 190005},
     {id: 4, name: 'walton4 phone', price: 190004},
     {id: 5, name: 'walton5', price: 190003},
-    {id: 8, name: 'walton6', price: 190002},
+    {id: 8, name: 'walton7', price: 190002},
+    {id: 9, name: 'walton68', price: 190002},
+    {id: 10, name: 'walton69', price: 190002},
+    {id: 11, name: 'walton60', price: 190002},
+    {id: 12, name: 'walton600 phone', price: 190002},
+    {id: 13, name: 'walton6000 Phone', price: 190002},
 ];
 for(const product of products){
     // console.log(product)
@@ -23,11 +28,11 @@ for(const product of products){
 function matchedProducts(products, search){
     const matched = [];
     for(const product of products){
-        if(product.name.includes(search)){
+        if(product.name.includes(search.toLowerCase())){
             matched.push(product)
         }
     }
     return matched
 }
-const result = matchedProducts(products, 'walton')
-console.log(result)
+const result = matchedProducts(products, 'phone')
+console.log(result, 'ss')
