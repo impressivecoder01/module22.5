04c62d3f032b9b11263219832fb3bfc75a3a399e
impressivecoder1 <rohanrahman251@gmail.com>
2025-10-30 22:21:12 +0600

    matching string

---
+++ class2.js
@@ -0,0 +1,33 @@
+//(advanced) find the matching product by searching products
+const numbers = [2,3,45,67,88,9];
+for(let i = 0; i<numbers.length; i ++){
+    const number = numbers[i]
+    console.log(number)
+}
+
+for(const number of numbers){
+    console.log(number,'s')
+}
+
+const products = [
+    {id: 1, name: 'walton phone', price: 190007},
+    {id: 2, name: 'walton2', price: 190006},
+    {id: 3, name: 'walton3', price: 190005},
+    {id: 4, name: 'walton4 phone', price: 190004},
+    {id: 5, name: 'walton5', price: 190003},
+    {id: 8, name: 'walton6', price: 190002},
+];
+for(const product of products){
+    // console.log(product)
+}
+function matchedProducts(products, search){
+    const matched = [];
+    for(const product of products){
+        if(product.name.includes(search)){
+            matched.push(product)
+        }
+    }
+    return matched
+}
+const result = matchedProducts(products, 'walton')
+console.log(result)
